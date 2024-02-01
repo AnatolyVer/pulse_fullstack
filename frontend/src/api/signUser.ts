5065d0e14435243a45445cdd7fb66e467a3c6cc3
AnatolyVer <tolik.vereshchak88@gmail.com>
2024-02-01 11:57:39 +0200

    1.02 - Deploy fixes

--- frontend/src/api/signUser.ts
+++ frontend/src/api/signUser.ts
@@ -4,8 +4,8 @@ import {IUserSign} from "@shared/interfaces/IUserSign.ts";
 
 export const signUser = async (link: string, formData: IUserSign) => {
     try {
-        const res = await axios.post(  `${import.meta.env.VITE_RESTAPI_DEV_URL}/user/${link}`, formData);
         console.log(`${import.meta.env.VITE_RESTAPI_DEV_URL}/user/${link}`)
+        const res = await axios.post(  `${import.meta.env.VITE_RESTAPI_DEV_URL}/user/${link}`, formData);
         localStorage.setItem("id", res.data)
         Cookies.set('access-token', res.headers['access-token'], { expires: 30 })
         Cookies.set('refresh-token', res.headers['refresh-token'], { expires: 40 })
