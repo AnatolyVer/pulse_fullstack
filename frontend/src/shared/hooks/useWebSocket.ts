3cd479107b93a0dd9c9edccfb0fc2015c6d0da95
AnatolyVer <tolik.vereshchak88@gmail.com>
2024-02-04 13:57:42 +0200

    4.02 - ws fixes

--- frontend/src/shared/hooks/useWebSocket.ts
+++ frontend/src/shared/hooks/useWebSocket.ts
@@ -22,6 +22,7 @@ const useWebSocket = () => {
             const payload:WSPayload = JSON.parse(event.data)
             switch (payload.type) {
                 case "NEW_MESSAGE":
+                    console.log(chat)
                     console.log(chat._id + " " + payload.body.chat_id)
                     if (chat._id === payload.body.chat_id) {
                         dispatch(addMessage(payload.body.message))
